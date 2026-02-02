2a0fe78d9fb66f40042d9df2ba40c97677ba6106
Exp <17885980+exp111@users.noreply.github.com>
2026-02-02 09:36:51 +0100

    kofi link

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {NgbCollapse} from '@ng-bootstrap/ng-bootstrap';
 import {ToastContainerComponent} from './toast-container/toast-container.component';
-import {GITHUB_URL} from './const';
+import {GITHUB_URL, KOFI_URL} from './const';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +13,5 @@ import {GITHUB_URL} from './const';
 export class App {
   isMenuCollapsed = true;
   protected readonly GITHUB_URL = GITHUB_URL;
+  protected readonly KOFI_URL = KOFI_URL;
 }
--- src/app/const.ts
+++ src/app/const.ts
@@ -5,6 +5,8 @@ export const IS_DEV = !environment.production;
 export const GITHUB_PAGES_URL = "https://exp111.github.io/MCCardle/";
 export const GITHUB_URL = "https://www.github.com/exp111/MCCardle";
 
+export const KOFI_URL = "https://ko-fi.com/exp111";
+
 export const MARVELCDB_BASE_URL = "https://marvelcdb.com";
 export const MARVELCDB_CARD_URL = "https://marvelcdb.com/card";
 
